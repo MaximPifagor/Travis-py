a5ec0656db60b16016a71393085e8626c9c51cc8
Misha Pryadun <49836838+DasPivas@users.noreply.github.com>
2019-11-17 21:57:33 +0500

    add new test

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -6,4 +6,7 @@ def test_fib_10():
 	assert(fibonacci(10) == 55)
 
 def test_fib_not_20():
-	assert(fibonacci(20) != 20)	
+	assert(fibonacci(20) != 20)
+	
+def test_fib_5():
+	assert(fibonacci(5) == 5)
